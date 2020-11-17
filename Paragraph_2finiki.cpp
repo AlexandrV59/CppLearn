c05c2b03b3abd31cee64d62817a1f684d9363d84
AlexandrV59 <kent1234810@gmail.com>
2020-11-17 12:43:24 +0500

    FinikiCount

---
+++ Paragraph_2finiki.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+//Finicky Counter
+int main()
+{
+    int count = 0;
+    while(true)
+    {
+        count++;
+        if (count > 10)
+        {
+            break;
+        }
+        if (count == 5)
+        {
+            continue;
+        }
+        cout << count << endl;
+    }
+}
